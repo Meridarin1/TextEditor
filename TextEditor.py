f4609db8dac8a356eea4ad3d59aac0abe0f6c815
Rafal Borkowski <meridarin@gamil.com>
2018-05-29 23:24:56 +0200

    Added Iconbar

--- TextEditor.py
+++ TextEditor.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import ttk
 import tkinter.filedialog
 import tkinter.messagebox
+from PIL import Image, ImageTk
 
 
 class TextEditor:
@@ -12,9 +13,11 @@ class TextEditor:
         root.title("Text Editor")
 
         #------Adding Text Area-----------------------------------
-        self.text_area = Text(root)
+        text_area_frame = Frame(root)
+        self.text_area = Text(text_area_frame)
         self.text_area.pack(side='left', fill='both', expand=True)
 
+        '''DELETED traditional Menu
         # ------Adding Menu---------------------------------------
         the_menu = Menu(root)
 
@@ -24,13 +27,43 @@ class TextEditor:
         file_menu.add_command(label='Save', command=self.save_file)
         file_menu.add_command(label='Quit', command=self.quit_app)
         the_menu.add_cascade(label="File", menu=file_menu)
-
+        '''
         # ------Adding Scrollbar-----------------------------------
         scrollbar = Scrollbar(root)
         scrollbar.config(command=self.text_area.yview)
         scrollbar.pack(side='right', fill='y')
 
-        root.config(menu=the_menu)
+        #-------Adding IconBar-------------------------------------
+        size = [24, 24]
+        toolbar = Frame(root, relief=RAISED)
+
+        open_img = Image.open("open.png")
+        save_img = Image.open("save.png")
+        exit_img = Image.open("exit.png")
+
+        open_img.thumbnail(size)
+        save_img.thumbnail(size)
+        exit_img.thumbnail(size)
+
+        open_icon = ImageTk.PhotoImage(open_img)
+        save_icon = ImageTk.PhotoImage(save_img)
+        exit_icon = ImageTk.PhotoImage(exit_img)
+
+        open_button = Button(toolbar, image=open_icon, command=self.open_file)
+        save_button = Button(toolbar, image=save_icon, command=self.save_file)
+        exit_button = Button(toolbar, image=exit_icon, command=self.quit_app)
+
+        open_button.image = open_icon
+        save_button.image = save_icon
+        exit_button.image = exit_icon
+
+        open_button.pack(side=LEFT, padx=2, pady=2)
+        save_button.pack(side=LEFT, padx=2, pady=2)
+        exit_button.pack(side=LEFT, padx=2, pady=2)
+
+        toolbar.pack(side=TOP, fill=X)
+        text_area_frame.pack(side=BOTTOM)
+        #root.config(menu=the_menu)
 
     def quit_app(self):
         self.root.quit()
